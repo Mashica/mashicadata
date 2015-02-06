c32c0f403249501459a8eb8a4a9d3fed43dafedc
Richard Stoutner <richard@mashica.com>
2015-02-05 21:54:01 -0600

    second Strava Test.

--- app/views/strava/index.blade.php
+++ app/views/strava/index.blade.php
@@ -6,7 +6,7 @@
 		<h1>Strava</h1>	
 		
 
-		<p><a href="https://www.strava.com/oauth/authorize?client_id=4647&response_type=code&redirect_uri=http://mashica.info/token_exchange&scope=public&approval_prompt=auto"><img src="/img/ConnectWithStrava@2x.png" alt="Enlace con Strava" style="height:30px;" border="0"></a></p>
+		<p><a href="https://www.strava.com/oauth/authorize?client_id=4647&response_type=code&redirect_uri=http://mashica.info/strava/token_exchange&scope=public&approval_prompt=auto"><img src="/img/ConnectWithStrava@2x.png" alt="Enlace con Strava" style="height:30px;" border="0"></a></p>
 
 
 @stop
